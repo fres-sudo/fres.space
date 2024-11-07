ed9d62ba0b47a199eb826f1a258d1722b630e6eb
fres-sudo <francescocalicchio@hotmail.com>
2024-11-07 23:39:43 +0100

    [FIX] Sveltekit adapter fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-auto";
 import { mdsvex } from "mdsvex";
 
 /** @type {import('mdsvex').MdsvexOptions} */
